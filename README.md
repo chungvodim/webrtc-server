7575908badc1a79508d0a775ffa2e11188edb364
Nguyen Nguyen Long <ba198647@gmail.com>
2020-03-12 19:08:43 +0700

    Try another source

--- README.md
+++ README.md
@@ -1,16 +1,7 @@
-## Install On Your Own Website
+##Running the signaling server
+```csharp
+git clone https://github.com/avspeed/RTCSignaling.io.git
 
-```sh
-mkdir demo && cd demo
-
-# install from NPM
-npm install rtcmulticonnection
-
-# or clone from github
-git clone https://github.com/chungvodim/rtc-service.git ./
-
-# install all required packages
-# you can optionally include --save-dev
 npm install
 
 node server --port=9001
--- server.js
+++ server.js
@@ -1,145 +1,41 @@
-// http://127.0.0.1:9001
-// http://localhost:9001
-// socket.io server <=> signaling server
+// Muaz Khan      - www.MuazKhan.com
+// MIT License    - www.WebRTC-Experiment.com/licence
 
-const fs = require('fs');
-const path = require('path');
-const url = require('url');
-var httpServer = require('http');
+// 2019-02-13 updated by gaetan siry to work with the iConfRTC SDK 
+// AVSPEED RTC Signaling Server
+// info@avspeed.com
 
-const ioServer = require('socket.io');
-const RTCMultiConnectionServer = require('rtcmulticonnection-server');
+console.log(process.argv);
 
-var PORT = 9001;
-var isUseHTTPs = false;
-
-const jsonPath = {
-    config: 'config.json',
-    logs: 'logs.json'
-};
-
-const BASH_COLORS_HELPER = RTCMultiConnectionServer.BASH_COLORS_HELPER;
-const getValuesFromConfigJson = RTCMultiConnectionServer.getValuesFromConfigJson;
-const getBashParameters = RTCMultiConnectionServer.getBashParameters;
-const resolveURL = RTCMultiConnectionServer.resolveURL;
-
-var config = getValuesFromConfigJson(jsonPath);
-config = getBashParameters(config, BASH_COLORS_HELPER);
-
-// if user didn't modifed "PORT" object
-// then read value from "config.json"
-if(PORT === 9001) {
-    PORT = config.port;
-}
-if(isUseHTTPs === false) {
-    isUseHTTPs = config.isUseHTTPs;
-}
+var isUseHTTPs = !(!!process.env.PORT || !!process.env.IP);
+console.log(process.env.PORT);
+console.log('https ' + isUseHTTPs);
+isUseHTTPs = false;
+var server = require(isUseHTTPs ? 'https' : 'http'),
+    url = require('url'),
+    path = require('path'),
+    fs = require('fs');
 
 function serverHandler(request, response) {
-    // to make sure we always get valid info from json file
-    // even if external codes are overriding it
-    config = getValuesFromConfigJson(jsonPath);
-    config = getBashParameters(config, BASH_COLORS_HELPER);
+    var uri = url.parse(request.url).pathname,
+        filename = path.join(process.cwd(), uri);
 
-    // HTTP_GET handling code goes below
+    var stats;
+
     try {
-        var uri, filename;
-
-        try {
-            if (!config.dirPath || !config.dirPath.length) {
-                config.dirPath = null;
-            }
-
-            uri = url.parse(request.url).pathname;
-            filename = path.join(config.dirPath ? resolveURL(config.dirPath) : process.cwd(), uri);
-        } catch (e) {
-            pushLogs(config, 'url.parse', e);
-        }
-
-        filename = (filename || '').toString();
-
-        if (request.method !== 'GET' || uri.indexOf('..') !== -1) {
-            try {
-                response.writeHead(401, {
-                    'Content-Type': 'text/plain'
-                });
-                response.write('401 Unauthorized: ' + path.join('/', uri) + '\n');
-                response.end();
-                return;
-            } catch (e) {
-                pushLogs(config, '!GET or ..', e);
-            }
-        }
-
-        if(filename.indexOf(resolveURL('/admin/')) !== -1 && config.enableAdmin !== true) {
-            try {
-                response.writeHead(401, {
-                    'Content-Type': 'text/plain'
-                });
-                response.write('401 Unauthorized: ' + path.join('/', uri) + '\n');
-                response.end();
-                return;
-            } catch (e) {
-                pushLogs(config, '!GET or ..', e);
-            }
-            return;
-        }
-
-        var matched = false;
-        ['/demos/', '/dev/', '/dist/', '/socket.io/', '/node_modules/canvas-designer/', '/admin/'].forEach(function(item) {
-            if (filename.indexOf(resolveURL(item)) !== -1) {
-                matched = true;
-            }
+        stats = fs.lstatSync(filename);
+    } catch (e) {
+        response.writeHead(404, {
+            'Content-Type': 'text/plain'
         });
+        response.write('404 Not Found:' + path.join('/', uri) + '\n');
+        response.end();
+        return;
+    }
 
-        // files from node_modules
-        ['RecordRTC.js', 'FileBufferReader.js', 'getStats.js', 'getScreenId.js', 'adapter.js', 'MultiStreamsMixer.js'].forEach(function(item) {
-            if (filename.indexOf(resolveURL('/node_modules/')) !== -1 && filename.indexOf(resolveURL(item)) !== -1) {
-                matched = true;
-            }
-        });
-
-        if (filename.search(/.js|.json/g) !== -1 && !matched) {
-            try {
-                response.writeHead(404, {
-                    'Content-Type': 'text/plain'
-                });
-                response.write('404 Not Found: ' + path.join('/', uri) + '\n');
-                response.end();
-                return;
-            } catch (e) {
-                pushLogs(config, '404 Not Found', e);
-            }
-        }
-
-        ['Video-Broadcasting', 'Screen-Sharing', 'Switch-Cameras'].forEach(function(fname) {
-            try {
-                if (filename.indexOf(fname + '.html') !== -1) {
-                    filename = filename.replace(fname + '.html', fname.toLowerCase() + '.html');
-                }
-            } catch (e) {
-                pushLogs(config, 'forEach', e);
-            }
-        });
-
-        var stats;
-
-        try {
-            stats = fs.lstatSync(filename);
-
-            if (filename.search(/demos/g) === -1 && filename.search(/admin/g) === -1 && stats.isDirectory() && config.homePage === '/demos/index.html') {
-                if (response.redirect) {
-                    response.redirect('/demos/');
-                } else {
-                    response.writeHead(301, {
-                        'Location': '/demos/'
-                    });
-                }
-                response.end();
-                return;
-            }
-        } catch (e) {
-            response.writeHead(404, {
+    fs.readFile(filename, 'binary', function (err, file) {
+        if (err) {
+            response.writeHead(500, {
                 'Content-Type': 'text/plain'
             });
             response.write('404 Not Found: ' + path.join('/', uri) + '\n');
@@ -147,146 +43,45 @@ function serverHandler(request, response) {
             return;
         }
 
-        try {
-            if (fs.statSync(filename).isDirectory()) {
-                response.writeHead(404, {
-                    'Content-Type': 'text/html'
-                });
-
-                if (filename.indexOf(resolveURL('/demos/MultiRTC/')) !== -1) {
-                    filename = filename.replace(resolveURL('/demos/MultiRTC/'), '');
-                    filename += resolveURL('/demos/MultiRTC/index.html');
-                } else if (filename.indexOf(resolveURL('/admin/')) !== -1) {
-                    filename = filename.replace(resolveURL('/admin/'), '');
-                    filename += resolveURL('/admin/index.html');
-                } else if (filename.indexOf(resolveURL('/demos/dashboard/')) !== -1) {
-                    filename = filename.replace(resolveURL('/demos/dashboard/'), '');
-                    filename += resolveURL('/demos/dashboard/index.html');
-                } else if (filename.indexOf(resolveURL('/demos/video-conference/')) !== -1) {
-                    filename = filename.replace(resolveURL('/demos/video-conference/'), '');
-                    filename += resolveURL('/demos/video-conference/index.html');
-                } else if (filename.indexOf(resolveURL('/demos')) !== -1) {
-                    filename = filename.replace(resolveURL('/demos/'), '');
-                    filename = filename.replace(resolveURL('/demos'), '');
-                    filename += resolveURL('/demos/index.html');
-                } else {
-                    filename += resolveURL(config.homePage);
-                }
-            }
-        } catch (e) {
-            pushLogs(config, 'statSync.isDirectory', e);
-        }
-
-        var contentType = 'text/plain';
-        if (filename.toLowerCase().indexOf('.html') !== -1) {
-            contentType = 'text/html';
-        }
-        if (filename.toLowerCase().indexOf('.css') !== -1) {
-            contentType = 'text/css';
-        }
-        if (filename.toLowerCase().indexOf('.png') !== -1) {
-            contentType = 'image/png';
-        }
-
-        fs.readFile(filename, 'binary', function(err, file) {
-            if (err) {
-                response.writeHead(500, {
-                    'Content-Type': 'text/plain'
-                });
-                response.write('404 Not Found: ' + path.join('/', uri) + '\n');
-                response.end();
-                return;
-            }
-
-            try {
-                file = file.replace('connection.socketURL = \'/\';', 'connection.socketURL = \'' + config.socketURL + '\';');
-            } catch (e) {}
-
-            response.writeHead(200, {
-                'Content-Type': contentType
-            });
-            response.write(file, 'binary');
-            response.end();
-        });
-    } catch (e) {
-        pushLogs(config, 'Unexpected', e);
-
-        response.writeHead(404, {
-            'Content-Type': 'text/plain'
-        });
-        response.write('404 Not Found: Unexpected error.\n' + e.message + '\n\n' + e.stack);
+        response.writeHead(200);
+        response.write(file, 'binary');
         response.end();
-    }
+    });
 }
 
-var httpApp;
+var app;
 
-if (isUseHTTPs) {
-    httpServer = require('https');
-
-    // See how to use a valid certificate:
-    // https://github.com/muaz-khan/WebRTC-Experiment/issues/62
+/*if (isUseHTTPs) {
     var options = {
-        key: null,
-        cert: null,
-        ca: null
+        key: fs.readFileSync(path.join(__dirname, 'fake-keys/privatekey.pem')),
+        cert: fs.readFileSync(path.join(__dirname, 'fake-keys/certificate.pem'))
     };
+    app = server.createServer(options, serverHandler);
+} else */
 
-    var pfx = false;
+app = server.createServer(serverHandler);
 
-    if (!fs.existsSync(config.sslKey)) {
-        console.log(BASH_COLORS_HELPER.getRedFG(), 'sslKey:\t ' + config.sslKey + ' does not exist.');
-    } else {
-        pfx = config.sslKey.indexOf('.pfx') !== -1;
-        options.key = fs.readFileSync(config.sslKey);
-    }
+app = app.listen(process.env.PORT || 9000, process.env.IP || "0.0.0.0", function () {
+    var addr = app.address();
 
-    if (!fs.existsSync(config.sslCert)) {
-        console.log(BASH_COLORS_HELPER.getRedFG(), 'sslCert:\t ' + config.sslCert + ' does not exist.');
-    } else {
-        options.cert = fs.readFileSync(config.sslCert);
-    }
-
-    if (config.sslCabundle) {
-        if (!fs.existsSync(config.sslCabundle)) {
-            console.log(BASH_COLORS_HELPER.getRedFG(), 'sslCabundle:\t ' + config.sslCabundle + ' does not exist.');
-        }
-
-        options.ca = fs.readFileSync(config.sslCabundle);
-    }
-
-    if (pfx === true) {
-        options = {
-            pfx: sslKey
-        };
-    }
-
-    httpApp = httpServer.createServer(options, serverHandler);
-} else {
-    httpApp = httpServer.createServer(serverHandler);
-}
-
-RTCMultiConnectionServer.beforeHttpListen(httpApp, config);
-httpApp = httpApp.listen(process.env.PORT || PORT, process.env.IP || "0.0.0.0", function() {
-    RTCMultiConnectionServer.afterHttpListen(httpApp, config);
+    console.log("AVSPEED RTC Signaling Server listening at ", addr.address + ":" + addr.port);
 });
 
-// --------------------------
-// socket.io codes goes below
+require('./underscore-min.js');
 
-ioServer(httpApp).on('connection', function(socket) {
-    RTCMultiConnectionServer.addSocket(socket, config);
+require('./Signaling-Server.js')(app, function (socket) {
+    try {
+        var params = socket.handshake.query;
 
-    // ----------------------
-    // below code is optional
+        if (!params.socketCustomEvent) {
+            params.socketCustomEvent = 'custom-message';
+        }
 
-    const params = socket.handshake.query;
-
-    if (!params.socketCustomEvent) {
-        params.socketCustomEvent = 'custom-message';
-    }
-
-    socket.on(params.socketCustomEvent, function(message) {
-        socket.broadcast.emit(params.socketCustomEvent, message);
-    });
+        socket.on(params.socketCustomEvent, function (message) {
+            try {
+                console.log(message);
+                socket.broadcast.emit(params.socketCustomEvent, message);
+            } catch (e) { }
+        });
+    } catch (e) { }
 });
